bd76f50f3329cfed11f7d7095dac3f69a19b4530
unknown <dsv19621227@gmail.com>
2024-05-23 16:06:58 +0300

    todo without Redux  first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+    
+  const addTodo = () => {
+    if(text.trim().length) {
+      setTodos([...todos, {
+        id: new Date().toISOString(),
+        text,
+        completed: false,
+      }]);
+    }
+    setText('');
+  }
+
+  const handleDelete =(id) => {
+    if (todos) 
+      setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
   return (
     <div className="App">
       <div className="App-container">
-        App
+        <label>
+          <input value={text} onChange={e=>setText(e.target.value)}/>
+          <button onClick={addTodo}>Add todo</button>
+        </label>
+        <ul>
+          {todos.map(todo => (
+            <li key={todo.id}>
+              <input type="checkbox"/>
+              <span>{todo.text}</span>
+              <span onClick={() => handleDelete(todo.id)} className="delete">&times;</span>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
