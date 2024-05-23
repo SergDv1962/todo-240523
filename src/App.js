267f63e4035ea68f54142ced5168b332bbb7057b
unknown <dsv19621227@gmail.com>
2024-05-23 18:06:57 +0300

    todo without redux finish

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
 import { useState } from "react";
 import "./App.css";
+import TodoList from "./components/TodoList";
+import InputFiel from "./components/InputFiel";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [text, setText] = useState('');
-    
+  const [text, setText] = useState("");
+
   const addTodo = () => {
-    if(text.trim().length) {
-      setTodos([...todos, {
-        id: new Date().toISOString(),
-        text,
-        completed: false,
-      }]);
+    if (text.trim().length) {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text,
+          completed: false,
+        },
+      ]);
     }
-    setText('');
-  }
+    setText("");
+  };
 
-  const handleDelete =(id) => {
-    if (todos) 
-      setTodos(todos.filter((todo) => todo.id !== id));
-  }
+  const removeTodo = (todoId) => {
+    if (todos) setTodos(todos.filter((todo) => todo.id !== todoId));
+  };
 
+  const toggleTodoComplete = (todoId) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id !== todoId) return todo;
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      })
+    );
+  };
   return (
     <div className="App">
       <div className="App-container">
-        <label>
-          <input value={text} onChange={e=>setText(e.target.value)}/>
-          <button onClick={addTodo}>Add todo</button>
-        </label>
-        <ul>
-          {todos.map(todo => (
-            <li key={todo.id}>
-              <input type="checkbox"/>
-              <span>{todo.text}</span>
-              <span onClick={() => handleDelete(todo.id)} className="delete">&times;</span>
-            </li>
-          ))}
-        </ul>
+        <InputFiel text={text} handleInput={setText} handleSubmit={addTodo}/>
+        <TodoList
+          todos={todos}
+          toggleTodoComplete={toggleTodoComplete}
+          removeTodo={removeTodo}
+        />
       </div>
     </div>
   );
---
+++ src/components/InputFiel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InputFiel = ({ text, handleInput, handleSubmit }) => {
+  return (
+    <label>
+      <input value={text} onChange={(e) => handleInput(e.target.value)} />
+      <button onClick={handleSubmit}>Add todo</button>
+    </label>
+  );
+};
+
+export default InputFiel;
